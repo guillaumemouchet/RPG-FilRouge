19ef8a83a9a9fd0849a4b551b179275939a196b4
Guillaume Mouchet <guillaume.mouchet@he-arc.ch>
2021-04-12 15:27:10 +0200

    (DEV) : Version 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-#include "Hero.cpp"
-#include "Sword.cpp"
 #include <string>
+
+//TOUJOURS inclure les headers !!
+#include "hero.h"
+#include "sword.h"
+
 using namespace std;
 using namespace HE_ARC::RPG;
 
+
 int main()
 {
-    Hero Hero1(5,5,5,50.0,"Guillaume");
-    Hero Hero2(12,4,6,70.5,"Benjamin");
-    Hero Hero3;
-    Hero3.show();
-    Hero1.interact(Hero2);
-    cout << "Agility: " << Hero1.getAgility() << endl;  
-    Sword Sword1(24);
+    Sword Sword1(2);
+    Sword Sword2(3);
+    Hero hero1(5,5,5,50.0,"Guillaume", Sword1);
+    Hero hero2(12,4,6,70.5,"Benjamin", Sword2);
+    Hero hero3;
+    
+    hero3.show();
+    hero1.interact(hero2);
+
+    hero3.interact(hero1);
+    hero1.show();
+    hero2.show();
+
+    cout << "Agility: " << hero1.getAgility() << endl;
+
+    Sword Sword3(24);
     cout << "Damage : " << Sword1.getDamage() << endl;
-    Hero2.interact(Hero1);
-    Hero1.show();
-    Hero2.show();
 
     return 0;
 }
