a057fcd2cbf18ad95a0f250ad201dfbb73623814
Guillaume Mouchet <guillaume.mouchet@he-arc.ch>
2021-04-14 11:21:32 +0200

    (DEV) : Création d'arborescence

---
+++ RPG/libs/actors/include/Wizard.h
@@ -0,0 +1,22 @@
+#ifndef WIZARD_H
+#define WIZARD_H
+#include "Hero.h"
+#include <iostream>
+using namespace std;
+namespace HE_ARC::RPG
+{
+    class Wizard : public Hero
+    {
+        protected:
+        int mana;
+        public:
+        Wizard(int _strength, int _agility, int _intelligence, double _hp, string _name, Sword _sword, int _mana);
+        Wizard();
+        void interact(const Hero &) override;
+        void castSpell();
+        void show();
+
+    };
+    
+}
+#endif
---
+++ RPG/libs/weapons/src/sword.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//TOUJOURS inclure les headers !!
+#include "..\include\sword.h"
+
+using namespace std;
+
+
+namespace HE_ARC::RPG
+{
+
+}
